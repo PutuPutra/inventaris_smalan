2412e97c304c49af2e87a5ac98a2e60f87666f37
PutuPutra <putupersada@gmail.com>
2023-01-02 18:32:02 +0700

    putu

--- app/Views/layout/templateAdmin.php
+++ app/Views/layout/templateAdmin.php
@@ -91,9 +91,49 @@
                 </div>
             </div>
         </div>
-        <?= $this->renderSection('content'); ?>
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
 
-        <!-- <div class="fixed-bottom">
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row">
+                        <div class="col-12 col-md-6 order-md-1 order-last">
+                            <h3>Dashboard</h3>
+                            <p class="text-subtitle text-muted"></p>
+                        </div>
+                        <div class="col-12 col-md-6 order-md-2 order-first">
+                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                                <ol class="breadcrumb" style="margin-left: -100px;">
+
+                                    <?php if (in_groups('super_admin')) : ?>
+                                    <li class="breadcrumb-item active" aria-current="page">
+                                        <a href="<?= base_url('account') ?>">
+                                            <i class="fa-solid fa-user"></i>
+                                        </a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">
+                                        <a href="<?= base_url('settings') ?>">
+                                            <i class="fa-solid fa-gear"></i>
+                                        </a>
+                                    </li>
+                                    <?php endif; ?>
+                                    <li class="breadcrumb-item">
+                                        <a href="<?= base_url('logout') ?>">
+                                            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                                        </a>
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <?= $this->renderSection('content'); ?>
+
+                <!-- <div class="fixed-bottom">
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
@@ -112,14 +152,14 @@
                 </div>
             </footer>
         </div> -->
-    </div>
-    </div>
-    <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/app.js"></script>
+            </div>
+        </div>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/app.js"></script>
 
-    <!-- Need: Apexcharts -->
-    <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
-    <script src="assets/js/pages/dashboard.js"></script>
+        <!-- Need: Apexcharts -->
+        <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+        <script src="assets/js/pages/dashboard.js"></script>
 
 </body>
 
